27a040af434e24c7c13848169eb44708e9326ba0
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2023-07-07 20:18:11 +0545

    adding new features

---
+++ database/expense.js
@@ -0,0 +1,27 @@
+let expenses=[
+    {
+        "_id":1,
+        "title":"food",
+        "paidBy":"Sonika",
+        "description":"Lunch in Restaurant",
+        "amount":2000
+
+    },
+    {
+        "_id":2,
+        "title":"transport",
+        "paidBy":"Ayesha",
+        "description":"Compute fare from Butwal to Pokhara",
+        "amount":3000
+
+    },
+    {
+        "_id":3,
+        "title":"hotel",
+        "paidBy":"Suvu",
+        "description":"Hotel dreamlanh fare charge",
+        "amount":5000
+
+    }
+]
+module.exports=expenses;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var expenses= require('../database/expense')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',number:20 });
+  res.render('index', { title: 'Expense Tracker',expenseList:expenses});
 });
-router.get('/wlit', function(req, res, next) {
-  res.render('index', { title: 'WLIT', number:10});
+router.get('/add', function(req, res, next) {
+  res.render('addExpense' );
+});
+router.post('/saveExpense', function(req, res, next) {
+let formData={
+  "title":req.body.title,
+ " paidBy":req.body.paidBy,
+ "description":req.body.description,
+ "amount":req.body.amount
+}
 
+expenses.push({...formData,_id:expenses.length+1})
+res.redirect('/')
 });
 
+router.post('/saveEdit/:id', function(req, res, next) {
+  let formData={
+  "title":req.body.title,
+ "paidBy":req.body.paidBy,
+ "description":req.body.description,
+ "amount":req.body.amount
+}
+  const index=expenses.findIndex(expenses=>{
+    return expenses._id == req.params.id
+  });
+  expenses.splice(index,1,{_id:req.params.id,...formData})
+  res.redirect('/')
+  });
+
+
+router.get('/delete/:index',function(req,res,next){
+expenses.splice(req.params.index,1)
+res.redirect('/')
+});
+
+router.get('/edit/:id', function(req, res) {
+  const expense=expenses.find(expense=>expense._id == req.params.id)
+  res.render('editExpense',{expense:expense})
+});
 module.exports = router;
